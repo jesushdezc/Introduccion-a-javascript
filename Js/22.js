dc1ff674eab2952ec643577fe42b7d29ccce5940
jesushdezc <alejandro.hcolmenares@hotmail.com>
2024-03-27 23:39:54 -0700

    Avance

--- Js/22.js
+++ Js/22.js
@@ -1,8 +1,30 @@
 
-const puntaje = 1001;
+// const puntaje = 1000;
 
-if(puntaje !== 1000) {
-    console.log('Si el puntaje es 1000')
-} else {
-    console.log('No es igual')
+// if(puntaje !== 1000) {
+//     console.log('No es igual')
+// } else {
+//     console.log('Si el puntaje es 1000')
+    
+// }
+
+// const efectivo = 1000;
+// const carrito = 800;
+
+// if ( efectivo < carrito) {
+//     console.log('El usuario puede pagar')
+// } else {
+//     console.log('Fondos insuficientes')
+// }
+
+const rol = 'EDITOR';
+
+if(rol === 'ADMINISTRADOR') {
+    console.log('Acceso a todo el sistema');
+}
+else if(rol === 'EDITOR') {
+    console.log('Eres editor, puedes entrar pero no puedes hacer mucho')
+}
+else {
+    console.log('No tienes acceso');
 }
---
+++ Js/23.js
@@ -0,0 +1,18 @@
+const metodoPago = 'Efectivo';
+
+switch(metodoPago) {
+    case 'tarjeta':
+        console.log('Pagaste con tarjeta');
+        break;
+    case 'Cheque':
+        console.log('El usuario va pagar con cheque, revisaremos los fondos primero');
+        break;
+    case 'Efectivo':
+            console.log('Pagaste con efectivo');
+            break;
+
+    default:
+        console.log('Aun no has pagado');
+        break;
+
+}
